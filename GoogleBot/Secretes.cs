56f9da871d7f7f111db8e2e1b7df2d962207d317
Matthias Harzer <matthias.harzer03@gmail.com>
2021-12-24 12:27:30 +0100

    Delete Secretes.cs

--- GoogleBot/Secretes.cs
+++
@@ -1,9 +0,0 @@
-namespace GoogleBot
-{
-    public class Secretes
-    {
-        public static readonly string DiscordToken = @"DISCORD-BOT-TOKEN";
-        public static readonly string GoogleApiKey = @"GOOGLE-CLOUD-API-KEY";
-        public static readonly string SearchEngineID = @"GOOGLE-CUSTOMSEARCHENGINE-ID";
-    }
-}
