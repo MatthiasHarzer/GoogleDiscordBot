cbb26ae595e3279479fc9af24f4f2959d81f8a01
Matthias Harzer <matthias.harzer03@gmail.com>
2021-12-24 11:17:26 +0100

    Create Secretes.cs

---
+++ GoogleBot/Secretes.cs
@@ -0,0 +1,9 @@
+namespace GoogleBot
+{
+    public class Secretes
+    {
+        public static readonly string DiscordToken = @"DISCORD-BOT-TOKEN";
+        public static readonly string GoogleApiKey = @"GOOGLE-CLOUD-API-KEY";
+        public static readonly string SearchEngineID = @"GOOGLE-CUSTOMSEARCHENGINE-ID";
+    }
+}
